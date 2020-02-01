7246483b838b013f85d498e9cd42ffe049068d9e
Alehandro Persaud <andrew041994@gmail.com>
2020-02-01 18:57:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,9 +19,9 @@ WIN_COMBINATIONS = [
 
           puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
 
-          puts "#{@board} | #{@board} | #{@baord}"
+          puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
 
-          puts "#{@board} | #{@board} | #{@baord}"
+          puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
         end
 
 end
