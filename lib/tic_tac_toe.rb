130d2c9c5f8e3bb382e05abfb23fa3bdb435cd66
Alehandro Persaud <andrew041994@gmail.com>
2020-02-01 18:49:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,8 +15,8 @@ WIN_COMBINATIONS = [
           @board=[" ", " ", " ", " ", " ", " ", " ", " ", " "]
         end
 
-        def display_board
-          puts "#{board[0]} | #{board[1]} | #{baord[2]}"
+        def display_board 
+          puts "#{@board[0]} | #{@board[1]} | #{@baord[2]}"
 
           puts "#{@board[3]} | #{@board[4]} | #{@baord[5]}"
 
